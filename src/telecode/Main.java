c8ad829916336762ca051b158742fecae69e4c40
jhb96 <jhbmjo@gmail.com>
2020-02-20 20:35:00 +0100

    v1 - empezando codigo

--- src/telecode/Main.java
+++ src/telecode/Main.java
@@ -1,5 +1,9 @@
 package telecode;
 
+import java.util.HashMap;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,7 +11,31 @@ public class Main {
 		
 		System.out.println("hola");
 		System.out.println("hola2");
+		System.out.println("hola3");
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(0, "abc");
+		map.put(1, "def");
+		map.put(2, "ghi");
+		
+		String s  = "f";
+		 
+		 for (Integer key : map.keySet()) {
+		        System.out.println("Key = " + key);
+		        if (map.get(key).contains(s)){
+		        	
+		        }
+		    }
+		
+		
+		
+		 
+		
+			
+			
+		}
+		 
 
 	}
 
-}
--- src/telecode/objeto.java
+++ src/telecode/objeto.java
@@ -4,5 +4,7 @@ public class objeto {
 
 	public objeto(){
 		
+		
+		
 	}
 }
