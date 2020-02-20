a7648dcd4129f5d65419089775d3c1a4be2aa5fe
jhb96 <jhbmjo@gmail.com>
2020-02-20 19:10:05 +0100

    v1 - syso 2

---
+++ src/telecode/Main.java
@@ -0,0 +1,13 @@
+package telecode;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hola");
+		System.out.println("hola2");
+
+	}
+
+}
